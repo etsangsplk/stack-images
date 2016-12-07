ac41a69e0125cc5f9e5166ae4534e345a423c6d6
David Zuelke <dzuelke@gmail.com>
2016-12-07 23:06:14 +0100

    additional headers for common ruby and python package deps

--- heroku-16-build/bin/heroku-16-build.sh
+++ heroku-16-build/bin/heroku-16-build.sh
@@ -9,35 +9,56 @@ apt-get install -y --force-yes \
     autoconf \
     bison \
     build-essential \
-    libbind-dev \
+    libacl1-dev \
+    libapparmor-dev \
+    libapt-pkg-dev \
+    libattr1-dev \
+    libaudit-dev \
     libbsd-dev \
     libbz2-dev \
     libcairo2-dev \
+    libcap-dev \
     libcurl4-openssl-dev \
-    libevent-dev \
+    libdb-dev \
     libev-dev \
+    libevent-dev \
     libexif-dev \
     libffi-dev \
     libgcrypt20-dev \
     libgd-dev \
+    libgdbm-dev \
+    libgeoip-dev \
     libglib2.0-dev \
     libgnutls-dev \
     libicu-dev \
     libidn11-dev \
     libjpeg-dev \
+    libkeyutils-dev \
+    libkmod-dev \
     libkrb5-dev \
     libldap2-dev \
+    liblz4-dev \
+    libmagic-dev \
     libmagickwand-dev \
     libmcrypt-dev \
     libmemcached-dev \
     libmysqlclient-dev \
     libncurses5-dev \
+    libncursesw5-dev \
+    libnetpbm10-dev \
     libpam0g-dev \
+    libpopt-dev \
     libpq-dev \
     librabbitmq-dev \
     libreadline-dev \
+    librtmp-dev \
+    libselinux1-dev \
+    libsemanage1-dev \
     libssl-dev \
+    libsystemd-dev \
+    libudev-dev \
     libuv1-dev \
+    libwrap0-dev \
     libxml2-dev \
     libxslt-dev \
     libyaml-dev \
